a32eeb1c6ebaae0b407bd390a3d10dfa74321a42
valentin <valentin.vesperini@gmail.com>
2021-11-09 19:22:42 +0100

    Version 1

---
+++ html et css/Piquante/Web-Developer-P6/backend/routes/sauces.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+const saucesCtrl = require('../controllers/sauces')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+// Route pour créer une nouvelle sauce
+router.post('/', auth, multer, saucesCtrl.createThing);
+   
+// Route pour une seule sauce
+router.get('/:id', auth, saucesCtrl.getOneThing)
+  
+// Route pour toutes les sauces
+router.get('/', auth, saucesCtrl.getAllThings);
+  
+//Route pour modifier une sauce
+router.put('/:id', auth, multer, saucesCtrl.modifyThing);
+
+//Route pour supprimer une sauce
+router.delete('/:id', auth, saucesCtrl.deleteThing);
+
+module.exports = router
